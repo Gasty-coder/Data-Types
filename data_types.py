fb470e7a4861cad9bdc05ec99fda73c90e474875
Gasty-coder <agastya.gholkar@gmail.com>
2026-01-28 16:52:47 -0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,10 @@
+number = 67 #integer
+number1 = 67.67 #float
+name = "Agastya" #string
+result = False #boolean
+print("Data types of number variable is: ", type(number))
+print("Data types of number1 variable is: ", type(number1))
+print("Data types of name variable is: ", type(name))
+print("Data types of result variable is: ", type(result))
+print(float(number), type(number))
+print(int(number1), type(number1))
